9ef7cd59c446e8421f09dba133b5bb2646bc9309
chdr7177655 <138139706+chdr7177655@users.noreply.github.com>
2023-10-31 13:38:27 +0200

    .

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('hello nodejs');
+
+const http = require('http');
+//create a server object:
+http.createServer( (req, res) => {
+    res.write('Hello World!'); //write a response to the client
+    res.end(); //end the response
+  }).listen(4000); //the server object listens on port 8080
